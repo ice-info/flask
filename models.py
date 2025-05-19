cd258256651a3ced8269cf831a75068188a71e40
ice-info <t03ju24@abdn.ac.uk>
2025-05-19 17:21:29 +0100

    Update models.py

--- models.py
+++ models.py
@@ -1,16 +1,16 @@
-# models.py
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
 class Shoe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
-    brand = db.Column(db.String(50))
-    price = db.Column(db.Float)
-    size = db.Column(db.String(10))
-    stock = db.Column(db.Integer)
+    name = db.Column(db.String(100), nullable=False)
+    brand = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    size = db.Column(db.String(10), nullable=False)
+    stock = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
         return f'<Shoe {self.name}>'
+
 
